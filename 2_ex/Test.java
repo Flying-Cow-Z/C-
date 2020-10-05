4249c7c43d56f10f6717ec679d100c0bb94b6cfd
Flying-Cow-Z <2511767449@qq.com>
2020-10-05 21:58:35 +0800

    2_java

---
+++ 2_ex/Test.java
@@ -0,0 +1,10 @@
+public class Test  {
+	public static void main(String[] args) {
+		// int a = 10;
+		// long b = 20;
+		// int c = (int)(a + b);
+		int c = 10;
+		c = c++;
+		System.out.println(c);
+	}
+}
