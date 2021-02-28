fb750322092a291484e4d7bde80b62e968d9a356
Flying-Cow-Z <2511767449@qq.com>
2021-02-28 23:01:16 +0800

    递归返回非负整数，数字之和

---
+++ 递归返回数字之和/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+        public static int sum(int n) {
+        if (n == 0){
+            return 0;
+        }
+
+      return n%10 + sum(n/10);
+    }
+    public static void main(String[] args) {
+        int n= 1234;
+        System.out.println( sum(n));
+    }
+
+}
